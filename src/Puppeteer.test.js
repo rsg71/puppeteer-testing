f53c799401b8ea771db77adea3c373f35120daf8
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-09-20 17:40:46 -0400

    modify test to run with beforeall and afterall cleanup

--- src/Puppeteer.test.js
+++ src/Puppeteer.test.js
@@ -1,23 +1,22 @@
 const puppeteer = require("puppeteer");
 
 describe("testing testing", () => {
-    it("should render the page", () => {
 
-        (async () => {
-            console.log('opening browser');
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto("https://dorshinar.me");
-            const pageTitle = await page.title();
-            console.log(await page.title());
+    let browser, page;
 
-            expect(pageTitle).toBe('All posts | Dor Shinar');
-
-            // await page.screenshot({ path: "screenshot.png" });
-
-            await browser.close();
-        })();
-
-
+    beforeAll(async () => {
+        browser = await puppeteer.launch({
+            headless: true
+        });
+        page = await browser.newPage();
+        await page.goto("https://dorshinar.me", { waitUntil: "domcontentloaded" })
     })
+
+    test("should get correct page title", async () => {
+        const pageTitle = await page.title();
+
+        expect(pageTitle).toBe('All posts | Dor Shinar');
+    });
+
+    afterAll(() => browser.close());
 })
